c2441b8f70a813145c11c8c5a21aed7894d765c1
O-taxi <twitaxi024@gmail.com>
2026-09-22 00:57:12 +0900

    ディレクトリ構成の変更

--- autorefresh.js
+++
@@ -1,2 +0,0 @@
-// コンソールにスクリプトの動作開始を表示
-console.log(new Date().toLocaleString(), ": スクリプトが適用されています");
--- background.js
+++
@@ -1,7 +0,0 @@
-// 必要な初期化やグローバルイベントリスナーを設定
-console.log("バックグラウンドサービスワーカーが起動しました");
-
-importScripts(
-    "./background/hidePromotions.js",
-    "./background/autoRefresh.js",
-);
